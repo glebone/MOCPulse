62d795dca91397671a377bc279e47491be3be165
glebone <glebone@yandex.ru>
2015-07-03 16:56:09 +0300

    Added Swifty pods and tested Alamofire

--- MOCPulse/AppDelegate.swift
+++ MOCPulse/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Alamofire.request(.GET, "http://httpbin.org/get")
         return true
     }
 
