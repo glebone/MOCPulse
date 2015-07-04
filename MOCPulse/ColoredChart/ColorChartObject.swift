2fc171a68e54a5c72549626f1c11f7b65360e930
Ascoron <mr.paul.kovalenko@gmail.com>
2015-07-04 17:27:11 +0300

    color chart classes

---
+++ MOCPulse/ColoredChart/ColorChartObject.swift
@@ -0,0 +1,16 @@
+//
+//  ColorChartObject.swift
+//  MOCPulse
+//
+//  Created by Paul Kovalenko on 04.07.15.
+//  Copyright (c) 2015 MOC. All rights reserved.
+//
+
+import UIKit
+
+class ColorChartObject: NSObject {
+    
+    var color : UIColor = UIColor.clearColor()
+    var value : CGFloat = 0.0
+   
+}
