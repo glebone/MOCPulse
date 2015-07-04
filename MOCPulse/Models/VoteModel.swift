cb58dee5519c576fc2a0db3209afd48fcc34e312
walklineua <proger@glebs-Mac-mini.local>
2015-07-04 21:56:32 +0300

    Parsing VotesModel.

--- MOCPulse/Models/VoteModel.swift
+++ MOCPulse/Models/VoteModel.swift
@@ -16,42 +16,61 @@ enum VoteType : NSInteger {
 }
 
 class VoteModel : NSObject {
-    var id : NSInteger?
+    var id : String?
     var name : String?
     var type : VoteType = VoteType.Default
     var result : String?
+    var greenVotes : NSInteger?
+    var redVotes : NSInteger?
+    var yellowVotes : NSInteger?
+    var allUsers : NSInteger?
+    var voteUsers : NSInteger?
     
     override init () {
         super.init()
     }
     
-    init(id _id: NSInteger, name _name: String, type _type: VoteType, result _result:String) {
+    init(id _id: String, name _name: String, greenVotes _greenVotes: NSInteger, redVotes _redVotes: NSInteger, yellowVotes  _yellowVotes: NSInteger) {
         super.init()
         self.id = _id
-        self.name = _name
-        self.type = _type
-        self.result = _result;
+        self.name = _name;
+        self.greenVotes = _greenVotes;
+        self.redVotes = _redVotes;
+        self.yellowVotes = _yellowVotes;
+        
+        //self.type = _type;
+        //self.result = _result;
     }
     
     static func votes(_completion: (NSArray?) -> Void) -> Request {
-        return API.response(API.request(.GET, path: "votes", parameters: nil, headers: ["" : ""]),
+        return API.response(API.request(.GET, path: "http://192.168.4.63:8080/votes", parameters: nil, headers: ["auth_token" : "123123"]),
             success: { (object) -> Void in
                 var list: NSMutableArray = [];
-                for (index: String, subJson: JSON) in object {
+
+                for (index: String, subJson: JSON) in object["votes"]{
+
+                    var voteID = subJson["id"].string;
+                    var voteName = subJson["name"].string;
                     
-                    let voitsDict: Dictionary<String, JSON> = subJson[index].dictionaryValue
+                    var greenVotes = subJson["result"]["green"].intValue;
+                    var redVotes = subJson["result"]["red"].intValue;
+                    var yellowVotes = subJson["result"]["yellow"].intValue;
+                
+                    var allUsers = subJson["result"]["all_users"].intValue;
+                    var voteUsers = subJson["result"]["vote_users"].intValue;
                     
-                    var voteID : NSInteger = voitsDict["id"]!.intValue;
-                    var voteName : NSString = voitsDict["name"]!.stringValue
-                    var voteResult : NSString = voitsDict["result"]!.stringValue
-                    
-                    var vote: VoteModel = VoteModel(id: voteID, name: voteName as String, type: VoteType.Color, result: voteResult as String)
-                    
+                    var vote : VoteModel = VoteModel(id: voteID!, name: voteName!, greenVotes: greenVotes, redVotes: redVotes, yellowVotes: yellowVotes);
+                    vote.allUsers = allUsers;
+                    vote.voteUsers = voteUsers;
+                
                     list.addObject(vote);
                 }
                 _completion(list);
             },
             failure: { (error) -> Void in
+                var error2: NSError? = error
+
+                println(error2?.localizedDescription)
                 //
         });
     }
