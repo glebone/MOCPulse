f43c0aa9261baa7ff693a0a188574396110a17b3
Ascoron <mr.paul.kovalenko@gmail.com>
2015-07-04 17:22:40 +0300

    OAuth2 authorization

--- MOCPulse/ViewController.swift
+++ MOCPulse/ViewController.swift
@@ -25,27 +25,29 @@ class ViewController: UIViewController {
     
     func oauthAuthorization()
     {
-        let oauthswift2 = OAuth2Swift(
-            consumerKey: "",
-            consumerSecret: "",
-            authorizeUrl: "",
-            accessTokenUrl: "",
-            responseType: ""
+        let oauthswift = OAuth2Swift(
+            consumerKey:    "288c7689f3eb0d5426c434413a8711534cc781751a545e431af6f7f3aa8650ee",
+            consumerSecret: "0d88640d7e479c01cb37bff27cc08843e97d4b3d021e3d13449a377afeeec5f3",
+            authorizeUrl:   "http://192.168.4.121:3000/oauth/authorize",
+            accessTokenUrl: "http://192.168.4.121:3000/oauth/token",
+            responseType:   "code"
         )
+        let state: String = generateStateWithLength(20) as String
         
-        var urlExample = NSURL(string: "oauth-swift://oauth-callback/instagram")!
+        var callbackURL = NSURL(string: "oauth-swift://oauth-callback/MOCPulse")
         
-        oauthswift2.authorizeWithCallbackURL( urlExample,
-            scope: "",
-            state: "",
-            params: ["" : ""],
-            success: { (credential, response, parameters) -> Void in
-                println(response)
-        }) { (error) -> Void in
-            println(error.localizedDescription)
-        }
+        oauthswift.authorizeWithCallbackURL( callbackURL!, scope: "public", state: state, success: {
+            credential, response, parameters in
+            
+            var alert = UIAlertController(title: "Your token is:", message: credential.oauth_token, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            }, failure: {(error:NSError!) -> Void in
+                println(error.localizedDescription)
+        })
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
